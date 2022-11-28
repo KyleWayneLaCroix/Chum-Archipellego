c252ac2268fa188e60cc09e4fe4fa702e720138b
Kyle Wayne LaCroix <kylewlacroix@gmail.com>
2022-11-28 12:02:27 -0600

    regen encounterfiles
    easier than doing a conflict fix

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1,3 +1,160 @@
 ; Johto Pokémon in grass
 
 JohtoGrassWildMons:
+
+    def_grass_wildmons AWAKENING_BEACH
+    db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+    ; morn
+    db 3, YANMA
+    db 3, PIKIPEK
+    db 2, BOUNSWEET
+    db 3, TYMPOLE
+    db 4, TYMPOLE
+    db 3, EXEGGCUTE
+    db 2, EXEGGCUTE
+    db 5, SANDYGAST
+    ; day
+    db 3, PIKIPEK
+    db 3, BOUNSWEET
+    db 2, DEWPIDER
+    db 3, TYMPOLE
+    db 4, EXEGGCUTE
+    db 3, BLIPBUG
+    db 2, YANMA
+    db 5, SANDYGAST
+    ; nite
+    db 3, ORICORIO_SENSU
+    db 3, BLIPBUG
+    db 2, ZIGZAGOON
+    db 3, TYMPOLE
+    db 4, DEWPIDER
+    db 3, EXEGGCUTE
+    db 2, EXEGGCUTE
+    db 5, SANDYGAST
+    end_grass_wildmons
+
+    def_grass_wildmons ROUTE_A
+    db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+    ; morn
+    db 2, GRUBBIN
+    db 3, ROOKIDEE
+    db 4, LILLIPUP
+    db 2, BOUNSWEET
+    db 2, PIKIPEK
+    db 3, YUNGOOS
+    db 4, FARFETCH_D
+    db 4, FARFETCH_D
+    ; day
+    db 2, ROOKIDEE
+    db 3, YUNGOOS
+    db 4, BLIPBUG
+    db 2, GRUBBIN
+    db 2, BOUNSWEET
+    db 3, FARFETCH_D
+    db 4, LILLIPUP
+    db 4, ZIGZAGOON
+    ; nite
+    db 2, JOLTIK
+    db 3, ZIGZAGOON
+    db 4, ORICORIO_SENSU
+    db 2, PURRLOIN
+    db 2, JOLTIK
+    db 3, FARFETCH_D
+    db 4, BLIPBUG
+    db 4, BLIPBUG
+    end_grass_wildmons
+
+    def_grass_wildmons GEAR_CITY
+    db 15 percent, 15 percent, 15 percent ; encounter rates: morn/day/nite
+    ; morn
+    db 5, BURGELA
+    db 7, YUNGOOS
+    db 7, TRUBBISH
+    db 6, ROOKIDEE
+    db 8, LILLIPUP
+    db 6, LILLIPUP
+    db 7, ROCKRUFF
+    db 8, GRIMER
+    ; day
+    db 5, ROOKIDEE
+    db 7, YUNGOOS
+    db 7, BURGELA
+    db 6, TRUBBISH
+    db 8, LILLIPUP
+    db 6, LILLIPUP
+    db 7, ROCKRUFF
+    db 8, GRIMER
+    ; nite
+    db 5, ORICORIO_SENSU
+    db 7, BLIPBUG
+    db 7, TRUBBISH
+    db 6, ROCKRUFF
+    db 8, ZIGZAGOON
+    db 6, BURGELA
+    db 7, LILLIPUP
+    db 8, GRIMER
+    end_grass_wildmons
+
+    def_grass_wildmons CITY_GEAR_HQ_BASEMENT
+    db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+    ; morn
+    db 8, GASTLY
+    db 7, GASTLY
+    db 6, GASTLY
+    db 8, GASTLY
+    db 7, GASTLY
+    db 6, MISDREAVOUS
+    db 8, DREEPY
+    db 8, LITWICK
+    ; day
+    db 8, GASTLY
+    db 7, GASTLY
+    db 6, GASTLY
+    db 8, GASTLY
+    db 7, GASTLY
+    db 6, MISDREAVOUS
+    db 8, DREEPY
+    db 8, HONEDGE
+    ; nite
+    db 8, GASTLY
+    db 7, GASTLY
+    db 6, GASTLY
+    db 8, GASTLY
+    db 7, GASTLY
+    db 6, MISDREAVOUS
+    db 8, DREEPY
+    db 8, SPIRRIP
+    end_grass_wildmons
+
+    def_grass_wildmons NORTH_FOREST
+    db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+    ; morn
+    db 8, DEERLING
+    db 8, SHROOMISH
+    db 8, LOTAD
+    db 8, PIKIPEK
+    db 8, MORELULL
+    db 8, FARFETCH_D
+    db 8, FARFETCH_D
+    db 8, PONYTA
+    ; day
+    db 8, LOTAD
+    db 8, SHROOMISH
+    db 8, STANTLER
+    db 8, DEERLING
+    db 8, PIKIPEK
+    db 8, FARFETCH_D
+    db 8, FARFETCH_D
+    db 8, PONYTA
+    ; nite
+    db 8, SHROOMISH
+    db 8, MORELULL
+    db 8, STANTLER
+    db 8, ZIGZAGOON
+    db 8, BLIPBUG
+    db 8, BLIPBUG
+    db 8, DEERLING
+    db 8, PONYTA
+    end_grass_wildmons
+
+    db -1 ; end
