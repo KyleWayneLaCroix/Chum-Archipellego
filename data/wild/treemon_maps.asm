a51bc34d70983d2755e1358cd2f6327bfa4eed9d
Kyle Wayne LaCroix <kylewlacroix@gmail.com>
2022-12-14 23:32:55 -0600

    Update treemon_maps.asm

--- data/wild/treemon_maps.asm
+++ data/wild/treemon_maps.asm
@@ -5,6 +5,10 @@ ENDM
 
 TreeMonMaps:
 	treemon_map NEW_BARK_TOWN,        TREEMON_SET_CITY
+	treemon_map ROUTE_A,              TREEMON_SET_ROUTE
+	treemon_map GEAR_CITY,            TREEMON_SET_CITY
+	treemon_map NORTHERN_FOREST,      TREEMON_SET_FOREST
+	treemon_map ROUTE_B,              TREEMON_SET_ROUTE
 	db -1
 
 RockMonMaps:
