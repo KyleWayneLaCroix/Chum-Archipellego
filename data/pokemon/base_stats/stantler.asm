cc54540ad2c1781b51aff98de994f07e61d0f65c
Kyle Wayne LaCroix <kylewlacroix@gmail.com>
2022-12-22 18:51:15 -0600

    update mudbray and porygon moves, also some catchrates

--- data/pokemon/base_stats/stantler.asm
+++ data/pokemon/base_stats/stantler.asm
@@ -4,7 +4,7 @@
 	;   hp  atk  def  spd  sat  sdf
 
 	db NORMAL, NORMAL ; type
-	db 45 ; catch rate
+	db 115 ; catch rate
 	db 163 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
