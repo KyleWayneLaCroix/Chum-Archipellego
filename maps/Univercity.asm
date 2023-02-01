f881b569e5ec53fc36874b4429aece3deecd32ac
Kyle Wayne LaCroix <kylewlacroix@gmail.com>
2023-01-31 20:14:48 -0600

    add UNIVERCITY NPCs

--- maps/Univercity.asm
+++ maps/Univercity.asm
@@ -31,6 +31,154 @@ DormitorySignScript:
 TracksSignScript:
 	jumptext TracksSignText
 
+UnivercityWhiteGirlScript:
+	jumptextfaceplayer UnivercityWhiteGirlText
+
+UnivercityFisherScript:
+	jumptextfaceplayer UnivercityFisherText
+
+UnivercityDaisyScript:
+	jumptextfaceplayer UnivercityDaisyText
+
+UnivercityCooltrainerFScript:
+	jumptextfaceplayer UnivercityCooltrainerFText
+
+UnivercityCooltrainerMScript:
+	jumptextfaceplayer UnivercityCooltrainerMText
+
+UnivercityGentlemanScript:
+	jumptextfaceplayer UnivercityGentlemanText
+
+UnivercitySuperNerdScript:
+	jumptextfaceplayer UnivercitySuperNerdText
+
+UnivercityBugCatcherScript:
+	jumptextfaceplayer UnivercityBugCatcherText
+
+UnivercityRedScript:
+	jumptextfaceplayer UnivercityRedText
+
+UnivercityRedText:
+	text "I'm doing a build"
+	line "your own major"
+	cont "to major in #-"
+	cont "-MON Breeding."
+
+	para "Did you know we"
+	line "STILL don't know"
+	cont "how any #MON"
+	cont "reproduce?"
+
+	para "It's true!"
+	done
+
+UnivercityBugCatcherText:
+	text "I'm studying to"
+	line "be a #MON"
+	cont "ENTOMOLOGIST."
+
+	para "There are WAY"
+	line "fewer BUG #-"
+	cont "-MON than actual"
+	cont "BUGs, so it seems"
+	cont "doable!"
+	done
+
+UnivercitySuperNerdText:
+	text "I'm a commuter"
+	line "student!"
+	done
+
+UnivercityGentlemanText:
+	text "I wish there was"
+	line "just one road out"
+	cont "of here that did"
+	cont "not involve going"
+	cont "on a hike in tall"
+	cont "grass crawling"
+	cont "with #MON."
+
+	para "This is a new"
+	line "suit!"
+	done
+
+UnivercityCooltrainerMText:
+	text "There's just so"
+	line "much to do here!"
+
+	para "We have a gym, a"
+	line "bar, the dorm, a"
+	cont "church place,"
+
+	para "there's the ranch"
+	line "to the east, the"
+	cont "fishing to the"
+	cont "south, AND a"
+	cont "jungle & Volcano"
+	cont "to the north!"
+
+	para "I'm from PALETTE"
+	line "TOWN, so I'm used"
+	cont "to just 2 houses"
+	cont "and a laboratory."
+
+	para "This shit is"
+	line "crazy!"
+	done
+
+UnivercityCooltrainerFText:
+	text "I'm so tired."
+
+	para "I had to do a lot"
+	line "of shopping."
+
+	para "Normally, not a"
+	line "problem, but the"
+	cont "elevator is out."
+
+	para "Lugging bags of"
+	line "stuff up and down"
+	cont "five flights of"
+	cont "stairs gets to be"
+	cont "a bit much."
+	done
+
+UnivercityDaisyText:
+	text "I love that we"
+	line "have this little"
+	cont "field by the dorm"
+
+	para "It's so pretty!"
+	done
+
+UnivercityFisherText:
+	text "I'm what they"
+	line "call a mature"
+	cont "student, so I"
+	cont "need to get some"
+	cont "walking in to"
+	cont "stay spry."
+
+	para "When you get to"
+	line "be 27, you'll"
+	cont "understand."
+	done
+
+UnivercityWhiteGirlText:
+	text "UNIVERCITY has"
+	line "the only gym in"
+	cont "the ARCHIPELAGO!"
+
+	para "They only hire"
+	line "women, so I was"
+	cont "thinking about"
+	cont "joining, but it's"
+	cont "the only gym, so"
+	cont "any experience"
+	cont "wouldn't really"
+	cont "transfer."
+	done
+
 JSchoolSignText:
 	text "Edward J. Meeman"
 	line "Journalism School"
@@ -88,4 +236,13 @@ Univercity_MapEvents:
 
 	def_object_events
 ;	object_event x, y, sprite, movement, rx, ry, h1, h2, palette, type, range, script, event_flag
+	object_event 12, 25, SPRITE_RED, SPRITEMOVEDATA_WALK_UP_DOWN, 4, 4, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, UnivercityRedScript, 0
+	object_event 24, 25, SPRITE_DAISY, SPRITEMOVEDATA_WANDER, 4, 4, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, UnivercityDaisyScript, 0
+	object_event 16, 12, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WANDER, 2, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, UnivercityCooltrainerMScript, 0
+	object_event  6, 27, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_WANDER, 1, 3, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercityBugCatcherScript, 0
+	object_event 34, 31, SPRITE_BEAUTY, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 3, 3, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercityWhiteGirlScript, 0
+	object_event 36, 19, SPRITE_FISHER, SPRITEMOVEDATA_WALK_UP_DOWN, 8, 8, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercityFisherScript, 0
+	object_event 29,  6, SPRITE_GENTLEMAN, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 8, 8, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercityGentlemanScript, 0
+	object_event  8, 14, SPRITE_SUPER_NERD, SPRITEMOVEDATA_WANDER, 1, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercitySuperNerdScript, 0
+	object_event 22, 18, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercityCooltrainerFScript, 0
 	object_event 26, 28, SPRITE_FRUIT_TREE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnivercityFruitTree, 0
