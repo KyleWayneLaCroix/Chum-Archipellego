326c5828dc0ca9733d667f4277786210a4a981ae
Kyle Wayne LaCroix <kylewlacroix@gmail.com>
2023-07-05 09:22:54 -0500

    Add new spirrip line sprites
    Commissioned from peanut on fiverr
    https://www.fiverr.com/peanutella/make-a-generation-2-pokemon-sprite

--- gfx/pokemon/doomstone/anim_idle.asm
+++ gfx/pokemon/doomstone/anim_idle.asm
@@ -1,1 +1,6 @@
+	frame 4, 09
+	frame 5, 09
+	frame 4, 09
+	frame 5, 09
+	frame 6, 19
 	endanim
--- gfx/pokemon/spirrip/anim.asm
+++ gfx/pokemon/spirrip/anim.asm
@@ -1,1 +1,9 @@
+	frame 0, 09
+	frame 1, 09
+	frame 2, 09
+	frame 1, 09
+	frame 0, 14
+	frame 3, 11
+	frame 4, 20
+	frame 3, 09
 	endanim
